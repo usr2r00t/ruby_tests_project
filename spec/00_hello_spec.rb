35f8939ea8061e3ca96675bf5faff6f13d59cd89
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-19 23:50:09 +0200

    Final version

--- spec/00_hello_spec.rb
+++ spec/00_hello_spec.rb
@@ -1,17 +1,19 @@
 require_relative '../lib/00_hello'
 
-describe "the hello function" do
-  it "says hello" do
+describe "The hello function" do
+  #say_hello
+  it "Says hello" do
     expect(hello).to eq("Hello!")
   end
 end
+  #desc
 
-describe "the greet function" do
-  it "says hello to someone" do
+describe "The greet function" do
+  it "Says hello to someone" do
     expect(greet("Alice")).to eq("Hello, Alice!")
   end
 
-  it "says hello to someone else" do
+  it "Says hello to someone else" do
     expect(greet("Bob")).to eq("Hello, Bob!")
   end
 end
