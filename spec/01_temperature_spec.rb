95fe677f59e827f8a56e30f2e25dc5891e34b2df
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-19 23:38:36 +0200

    Update 01_temperature_spec.rb

--- spec/01_temperature_spec.rb
+++ spec/01_temperature_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/01_temperature'
 
+  #desc_&_my_temp_convertion
 describe "temperature conversion functions" do
 
   describe "#ftoc" do
@@ -7,23 +8,26 @@ describe "temperature conversion functions" do
     it "converts freezing temperature" do
       expect(ftoc(32)).to eq(0)
     end
-
+    
+  #boil_conv_temp  
     it "converts boiling temperature" do
       expect(ftoc(212)).to eq(100)
     end
-
+  
+  #body_conv_temp  
     it "converts body temperature" do
       expect(ftoc(98.6)).to eq(37)
     end
-
+    
+  #arb_temp_conv
     it "converts arbitrary temperature" do
       expect(ftoc(68)).to eq(20)
-    end
-
+    end 
   end
-
+ 
   describe "#ctof" do
-
+   
+   #it_freez
     it "converts freezing temperature" do
       expect(ctof(0)).to eq(32)
     end
@@ -35,16 +39,14 @@ describe "temperature conversion functions" do
     it "converts arbitrary temperature" do
       expect(ctof(20)).to eq(68)
     end
-
+    
+  #conv_body_temp    
     it "converts body temperature" do
       expect(ctof(37)).to be_within(0.1).of(98.6)
-      # Why do we need to use be_within?
-      # See http://www.ruby-forum.com/topic/169330
-      # and http://en.wikipedia.org/wiki/IEEE_754-2008
-      # and http://en.wikipedia.org/wiki/Double_precision_floating-point_format
-      # Also, try "puts 0.5 - 0.4 - 0.1" -- pretty crazy, right?
     end
 
   end
 
 end
+
+# Coded By ThomTiber for The_Hacking_Project
