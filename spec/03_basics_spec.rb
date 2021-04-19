71cb26629eeda2d6d358c9d19d41798b85b4cb32
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-19 23:53:52 +0200

    Final version

--- spec/03_basics_spec.rb
+++ spec/03_basics_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/03_basics'
 
 describe 'who is the biggest number' do
-  # Hint: 'def who_is_bigger(a, b, c)'
+
   it 'tells me the biggest' do
     expect(who_is_bigger(84, 42, nil)).to eq("nil detected")
     expect(who_is_bigger(nil, 42, 21)).to eq("nil detected")
@@ -12,8 +12,7 @@ describe 'who is the biggest number' do
 end
 
 describe 'crazy stuff on strings' do
-  # Reverse, upcase then removes all L, T and A.
-  # Hint: google ruby string
+
   it 'does crazy stuff on strings' do
     expect(reverse_upcase_noLTA("Tries this at Home, Kids"))
       .to eq("SDIK ,EMOH  SIH SEIR")
@@ -26,11 +25,6 @@ end
 
 
 describe '42 finder' do
-  # array_42 takes an array as parameter and returns:
-  # - true if there's a 42 in the array items
-  # - false otherwise
-  # Hint: Should be 2 lines (and can be one :)
-  # Hint: google ruby array each
   it 'finds 42' do
     expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])).to eq false
     expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 42, 21, 10.5])).to eq true
@@ -39,15 +33,7 @@ end
 
 
 describe 'crazy stuff on arrays' do
-  # The magic_array function takes an array of number or an array of
-  # array of number as parameter and return the same array :
-  # - flattened (i.e. no more arrays in array)
-  # - sorted
-  # - with each number multiplicated by 2
-  # - with each multiple of 3 removed
-  # - with each number duplicate removed (any number should appear only once)
-  # - sorted
-  # BONUS : You can do this in one line less than 55 chars
+
   it 'does crazy stuff on Arrays' do
     expect(magic_array([1, 2, 3, 4, 5, 6]))
       .to eq([2, 4, 8, 10])
