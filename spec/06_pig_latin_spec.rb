29a053660566e5efca2c5a136016cdc25fba8e6d
Thom Tiber <contact@thomdeveloppeur.com>
2021-04-19 23:51:07 +0200

    Final version

--- spec/06_pig_latin_spec.rb
+++ spec/06_pig_latin_spec.rb
@@ -46,8 +46,4 @@ describe "#translate" do
     expect(s).to eq("ethay ickquay ownbray oxfay")
   end
 
-  # Test-driving bonus:
-  # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
-  # * retain the punctuation from the original phrase
-
 end
